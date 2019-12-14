38ca78ace8e697ad369e159fbade57bbdb073d2b
satish <gsatish2013@gmail.com>
2019-12-14 11:55:49 +0530

    Inital project setup

---
+++ src/main/java/com/rabobank/demo/RebobankService/RebobankServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rabobank.demo.RebobankService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebobankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebobankServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rabobank/demo/RebobankService/RebobankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabobank.demo.RebobankService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebobankServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
